f08c24109db13500d5f94e9f991ff089ac341b17
wytsai7660 <wytsai7660@gmail.com>
2023-06-06 16:26:37 +0800

    uptate the welcome message

--- main.lua
+++ main.lua
@@ -5,7 +5,7 @@ local widget       = require("widget")
 
 Score              = 0
 
-local welcome      = display.newImage("images/welcome.png", display.contentCenterX, display.contentCenterY)
+local welcome      = display.newImage("images/welcome.jpg", display.contentCenterX, display.contentCenterY)
 local screen_ratio = display.contentHeight / display.contentWidth
 local pic_ratio    = welcome.height / welcome.width
 if pic_ratio > screen_ratio then
@@ -16,17 +16,19 @@ else
     welcome.width = welcome.height / pic_ratio
 end
 
-local button = widget.newButton {
-    defaultFile = "images/stone5.png",
-    onEvent = function(event)
-        if event.phase == "began" then
-            event.target.alpha = 0.75
-        elseif event.phase == "ended" or event.phase == "cancelled" then
-            event.target.alpha = 1
-            composer.gotoScene("main_game", { effect = "fade", time = 500 })
-        end
-    end
-}
+local character = display.newImage("images/character.png", display.contentCenterX, display.contentCenterY)
+character:scale(0.1, 0.1)
 
-button.x, button.y = display.contentCenterX, display.contentCenterY + 150
-button.width, button.height = 400, 400
+local button = widget.newButton {
+    defaultFile = "images/start_btn.png",
+    onPress = function(event)
+        event.target.alpha = 0.75
+    end,
+    onRelease = function(event)
+        event.target.alpha = 1
+        composer.gotoScene("main_game", { effect = "fade", time = 500 })
+    end,
+    x = display.contentCenterX,
+    y = display.contentCenterY + 125
+}
+button:scale(0.1, 0.1)
